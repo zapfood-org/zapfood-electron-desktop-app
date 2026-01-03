23b1ded5390ee45a0b43cc2ece09962141821f5b
romulogdonadoni <romulogdonadoni@gmail.com>
2026-01-02 23:39:49 -0300

    refactor: Simplify auth-client by removing unused organizationClient plugin from the authentication setup.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,8 @@
 import { AUTH_URL } from "@/config/api";
-import { organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
   baseURL: AUTH_URL,
-  plugins: [organizationClient()],
   fetch: (input: RequestInfo | URL, init: RequestInit) => {
     return fetch(input, {
       ...init,
